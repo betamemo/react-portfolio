52b8fce58979acd01c0e263fcf2039a49cb2828a
betamemo <59756355+betamemo@users.noreply.github.com>
2026-06-03 11:41:26 +0700

    Change details and icons

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import Styles from './Services.module.css'
-import { FaCode, FaPaintBrush, FaMobileAlt } from 'react-icons/fa'
+import { TbDeviceImacCode, TbChartHistogram, TbMoodCheck } from "react-icons/tb";
 
 function Services() {
     return (
         <div className={Styles.services_container}>
-            <h3 className={Styles.services_title}>My Services</h3>
+            <h3 className={Styles.services_title}>What I Do</h3>
             <div className={Styles.services_list}>
 
                 <div className={Styles.services_items}>
-                    <FaCode />
-                    <h4>Web Development</h4>
-                    <p>Creating responsive and user-friendly websites and web applications.</p>
+                    <TbDeviceImacCode />
+                    <h4>Software & Web Development</h4>
+                    <p>Creating robust and scalable web applications from intuitive frontends to powerful backends.</p>
                 </div>
 
                 <div className={Styles.services_items}>
-                    <FaPaintBrush />
-                    <h4>UI/UX Design</h4>
-                    <p>Designing intuitive and visually appealing user interfaces.</p>
+                    <TbChartHistogram />
+                    <h4>AI & Data-Driven Solutions</h4>
+                    <p>Enhancing digital experiences by integrating smart features, data reporting, and modern AI tools.</p>
                 </div>
 
                 <div className={Styles.services_items}>
-                    <FaMobileAlt />
-                    <h4>Frontend Consulting</h4>
-                    <p>Providing expert guidance on frontend development best practices and technologies.</p>
+                    <TbMoodCheck />
+                    <h4>Automation & Quality Assurance</h4>
+                    <p>Ensuring high-quality performance and reliability through automated testing frameworks and optimized workflows.</p>
                 </div>
 
             </div>
