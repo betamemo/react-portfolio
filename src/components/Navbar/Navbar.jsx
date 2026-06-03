deab824c8b5635ccd77fd6db7d4b08a6db939701
betamemo <59756355+betamemo@users.noreply.github.com>
2026-06-03 16:56:31 +0700

    Add responsive styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
+import { TbMenu2 } from 'react-icons/tb';
 
 function Navbar() {
+
+    const [isToggled, setToggled] = useState(false);
+
+    function toggleMenu() {
+        setToggled(!isToggled);
+    }
+
     return (
         <nav>
             <div className={styles.container}>
-                <div className={styles.navbar}>
+                <div className={styles.nav_container}>
+
                     <div className={styles.logo}>
                         <a href='#'>Portfolio</a>
                     </div>
 
                     <ul>
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>Experience</a></li>
+                        <li><a href='#'>Skills</a></li>
                         <li><a href='#'>Projects</a></li>
                         <li><a href='#'>Contact</a></li>
                     </ul>
@@ -21,6 +29,24 @@ function Navbar() {
                         <a href='#'>Say Hi!</a>
                     </div>
                 </div>
+
+                {/* Mobile Menu */}
+                <TbMenu2 className={styles.menu} onClick={toggleMenu} />
+
+                {isToggled ? (
+                    <>
+                        <ul className={styles.mobile_menu}>
+                            <li><a href='#'>Skills</a></li>
+                            <li><a href='#'>Projects</a></li>
+                            <li><a href='#'>Contact</a></li>
+                        </ul>
+
+                        <div className={styles.mobile_button}>
+                            <a href='#'>Say Hi!</a>
+                        </div>
+                    </>
+                ) : null}
+
             </div>
         </nav>
     )
